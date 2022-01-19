eea4ae14d602c686c7632e9a47886e65bef8ca57
Christian Vorwerk <vorwerk@uchicago.edu>
2022-01-19 17:40:27 -0600

    Add periodic_table.json from pymatgen.core since we need it to validate element names. First draft of tests.

--- pymatgen/io/qbox/qbox.py
+++ pymatgen/io/qbox/qbox.py
@@ -5,6 +5,8 @@
 Classes for reading/manipulating/writing qbox input files.
 """
 import numpy as np
+import json
+from pathlib import Path
 import scipy.constants as const
 from monty.io import zopen
 from monty.json import MSONable
@@ -21,11 +23,19 @@ __date__ = "Dec 22, 2021"
 
 bohr_in_A = const.value('Bohr radius') / 10**(-10)
 
+# Loads element data from json file
+# Same data as used in pymatgen.core.Element
+with open(str(Path(__file__).absolute().parent / "periodic_table.json")) as f:
+    _pt_data = json.load(f)
+
 class QboxInput(MSONable):
    
     def __init__(self, structure, pseudos=None):
         self.structure = structure
         if pseudos != None:
+            for species in self.structure.composition.keys():
+                if str(species) not in pseudos:
+                    raise QboxInputError("Missing %s in pseudo specification!" % species)
             self.pseudos = pseudos
     
     @staticmethod
@@ -49,6 +59,26 @@ class QboxInput(MSONable):
                 output = Element(key)
                 break
         return output
+    
+    @staticmethod
+    def _is_valid_long_name(longname: str) -> bool:
+        """
+        Returns true if symbol is a valid long name.
+        Args:
+            symbol (str): Element long name
+        Returns:
+            True if symbol is a valid long name (e.g., "Silicon"). False
+            otherwise (e.g., "Zebra").
+        """
+        if longname[0].islower():
+            longname = longname[0].upper() + longname[1:]
+        
+        val = False
+        for key in _pt_data.keys():
+            if _pt_data[key]["Name"] == longname:
+                val = True
+                break
+        return val
 
     @classmethod
     def from_file(cls, file):
@@ -75,7 +105,7 @@ class QboxInput(MSONable):
                 if line.split()[0] == 'species':
                     if Element.is_valid_symbol(line.split()[1]):
                         element_ = Element(line.split()[1])
-                    elif Element.is_valid_long_name(line.split()[1]):
+                    elif QboxInput._is_valid_long_name(line.split()[1]):
                         element_ = QboxInput._element_from_long_name(line.split()[1])
                     
                     pseudos[element_.symbol] = line.split()[-1]
@@ -89,7 +119,7 @@ class QboxInput(MSONable):
                 if 'atom' in line.split():
                     if Element.is_valid_symbol(line.split()[2]):
                         species_.append(Element(line.split()[2]))
-                    elif Element.is_valid_long_name(line.split()[2]):
+                    elif QboxInput._is_valid_long_name(line.split()[2]):
                         species_.append(QboxInput._element_from_long_name(line.split()[2]))
 
                     coords_.append([float(entry)*bohr_in_A for entry in line.split()[3:6]])
@@ -112,13 +142,13 @@ class QboxInput(MSONable):
         # add pseudopotential
         if self.pseudos != None:
             for key in self.pseudos.keys():
-                string += 'species '+Element(key).symbol+' '+ self.pseudos[key] + '\n'
+                string += 'species '+str(key)+' '+ self.pseudos[key] + '\n'
 
         # add atoms
         i = 0
         for site in self.structure.sites:
             i += 1
-            string += 'atom '+str(site.species)[:-1]+str(i) + ' ' + site.species_string + ' '   
+            string += 'atom '+site.species.elements[0].symbol+str(i) + ' ' + site.species_string + ' '   
             for entry in site.coords:
                 string += str(entry/bohr_in_A) + ' ' 
             string += '\n'
@@ -136,3 +166,9 @@ class QboxInput(MSONable):
             f.write(self.__str__())
 
 
+class QboxInputError(BaseException):
+    """
+    Error for QboxInput
+    """
+
+    pass
